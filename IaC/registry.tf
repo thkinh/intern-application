390c79400949c5f44b9440864aaeda608d33f265
thkinh <139554989+thkinh@users.noreply.github.com.>
2025-08-27 20:47:17 +0700

    fix: remove redundant privilege for sa

--- IaC/registry.tf
+++ IaC/registry.tf
@@ -5,13 +5,4 @@ resource "google_artifact_registry_repository" "my_registry" {
   format        = "DOCKER"
 }
 
-resource "google_project_iam_binding" "artifact_registry_writer" {
-  project = "thinhs-project"
-  role    = "roles/artifactregistry.writer"
-
-  members = [
-    "serviceAccount:thkinh@thinhs-project.iam.gserviceaccount.com",
-  ]
-}
-
 
