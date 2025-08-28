73424ed3dcc6db5c9b6a0e058131d7c456b731e1
thkinh <139554989+thkinh@users.noreply.github.com.>
2025-08-28 21:29:29 +0700

    add README.md, Todo list

---
+++ monitor_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+URL="https://simpleapp-1032571919249.asia-southeast2.run.app"
+
+count=0;
+
+trap 'echo -e "\nDone requesting\nRequests sent: ${count}"' EXIT
+
+while true; do
+  curl -s -o /dev/null -w "%{http_code}\n" "$URL"
+  ((count++))
+  sleep 1  
+done
+
+
