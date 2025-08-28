88aae7412003fb476cff66620006cda284dceac9
thkinh <139554989+thkinh@users.noreply.github.com.>
2025-08-28 11:33:52 +0700

    test: set NODE_ENV=test to prevent server from starting during Jest

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
-});
+if(process.env.NODE_ENV !== "test")
+{
+  app.listen(PORT, () => {
+    console.log(`✅ Server running at http://localhost:${PORT}`);
+  });
+}
 
 module.exports = app;
